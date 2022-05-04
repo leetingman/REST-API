465e23e373da85ac6228cca5655123941e5b1e43
leetingman <dwa08@naver.com>
2022-05-04 16:48:14 +0900

    Test exceptions

--- src/test/java/me/liting/restapiwithspring/accounts/AccountServiceTest.java
+++ src/test/java/me/liting/restapiwithspring/accounts/AccountServiceTest.java
@@ -1,17 +1,23 @@
 package me.liting.restapiwithspring.accounts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.internal.runners.statements.ExpectException;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 
+import static junit.framework.TestCase.fail;
 import static org.assertj.core.api.Assertions.assertThat;
 
 //Account Service Test
@@ -20,6 +26,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+    @Rule
+    public ExpectedException expectedException=ExpectedException.none();
+
+
     @Autowired
     AccountService accountService;
 
@@ -45,6 +55,28 @@ public class AccountServiceTest {
         //Then
         assertThat(userDetails.getPassword()).isEqualTo(password);
     }
+
+    @Test
+    public void findByUsernameFail(){
+        //Expected
+        String username ="random@gamil.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        //When
+        accountService.loadUserByUsername(username);
+
+//        try{
+//
+//            accountService.loadUserByUsername(username);
+//            fail("supposed to be failed");
+//        }catch(UsernameNotFoundException e)
+//        {
+//            assertThat(e.getMessage()).containsSequence(username);
+//
+//        }
+    }
+
 
 
 }
